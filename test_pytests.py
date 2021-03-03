6a9312c0ab4e0be2a2a54a6e67e0f315bde6bc8b
pcesarcfh <79610997+pcesarcfh@users.noreply.github.com>
2021-03-03 20:23:50 -0300

    Update test_pytests.py

--- test_pytests.py
+++ test_pytests.py
@@ -1,7 +1,13 @@
 import pytest
 from app import soma
+from app import subt
 
 
 @pytest.mark.parametrize('a,b,expected', [(1, 1, 2), (-1, 1, 0), (-1, -1, -2), (1.1, 1.1,2.2), (-1.2, -1.2,-2.4)])
 def test_soma(a,b, expected):
     assert soma(a,b) == expected
+
+@pytest.mark.parametrize('a,b,expected', [(1, 1, 0), (-1, 1, 0), (-1, -1, 2), (2.1, 1.0,1.1), (-1.2, -1.2,2.4)])
+def test_subt(a,b, expected):
+    assert subt
+    (a,b) == expected
